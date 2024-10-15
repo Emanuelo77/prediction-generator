6748f5d10680c4d4d4753ee850edabd3910f9898
Emanuel crisan <emanuelcrisan778@gmail.com>
2024-10-15 10:12:57 +0200

    Adding

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "role_name" {
+  description = "The name of the Lambda execution role."
+  type        = string
+ 
+}
+variable "policy_name" {
+  description = "the name of the AS managed Lambda execution role basic policy "
+ type = string
+  
+}
+
