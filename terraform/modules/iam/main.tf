f2f21ae5e6bb7eb254741307a4c3a82e6acc6cd8
Emanuel crisan <emanuelcrisan778@gmail.com>
2024-10-15 13:00:43 +0200

    adding

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -2,20 +2,26 @@ resource "aws_iam_role" "lambda_execution_role" {
     name = var.role_name
 
     assume_role_policy = jsonencode({
-      "Version": "2012-10-17",
-      "Statement": [
+    "Version": "2012-10-17",
+    "Statement": [
         {
-          "Effect": "Allow",
-          "Principal": {
-            "Service": "lambda.amazonaws.com"
-          },
-          "Action": "sts:AssumeRole"
+            "Effect": "Allow",
+            "Action": [
+                "sts:AssumeRole"
+            ],
+            "Principal": {
+                "Service": [
+                    "lambda.amazonaws.com"
+                ]
+            }
         }
-      ]
-    })
+    ]
+})
+  
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_execution_role_policy_attachment" {
-    role       = aws_iam_role.lambda_execution_role.name
+    role = aws_iam_role.lambda_execution_role.name
     policy_arn = "arn:aws:iam::aws:policy/service-role/${var.policy_name}"
+  
 }
