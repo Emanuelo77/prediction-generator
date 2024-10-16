4c04d8a1e1f14680aec082f8a12db885468c1881
Emanuel crisan <emanuelcrisan778@gmail.com>
2024-10-16 10:41:32 +0200

    adding to dynamodb

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
+const AWS = require ('aws-sdk');
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+
 exports.handler = async (event) => {
-  // TODO implement
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
-  };
-  return response;
+
+   const predictionID = 'pred-$ (Date.now())';
+   const { question, prrediction } = event;
+
+   const param = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      predictionID,
+      question,
+      prediction
+    }
+   };
+  try {
+    await dynamodb.put(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Prediction saved successfully' }),
+    };
+    
+  } catch (error) {
+     return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Error saving prediction', error }),
+    };
+  }
 };
