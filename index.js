c16e951971cf96af431b9d9fb54d33dba62c2ed0
Emanuel crisan <emanuelcrisan778@gmail.com>
2024-10-16 10:46:44 +0200

    3333

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-const AWS = require ('aws-sdk');
+const AWS = require('aws-sdk');
 const dynamodb = new AWS.DynamoDB.DocumentClient();
-
 
 exports.handler = async (event) => {
 
-   const predictionID = 'pred-$ (Date.now())';
-   const { question, prrediction } = event;
+    const predictionID = `pred-$(Date.now())`;
+    const { question, prediction } = event;
 
-   const param = {
-    TableName: process.env.DYNAMODB_TABLE,
-    Item: {
-      predictionID,
-      question,
-      prediction
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE,
+      Item: {
+        predictionID,
+        question,
+        prediction
+      }
+    };
+
+    try {
+      await dynamodb.put(params).promise();
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Prediction saved successfully", predictionID })
+      };
+    } catch (error) {
+        return {
+          statusCode: 500,
+          body: JSON.stringify({ message: "Error saving prediction", error })
+        }; 
     }
-   };
-  try {
-    await dynamodb.put(params).promise();
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Prediction saved successfully' }),
-    };
-    
-  } catch (error) {
-     return {
-      statusCode: 500,
-      body: JSON.stringify({ message: 'Error saving prediction', error }),
-    };
-  }
 };
